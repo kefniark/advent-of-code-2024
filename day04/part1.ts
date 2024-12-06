76c9739277e747deeb06c598d51be90b74bb3e50
Kevin Destrem <kefniark@gmail.com>
2024-12-06 01:26:24 +0000

    more linting

--- day04/part1.ts
+++ day04/part1.ts
@@ -4,15 +4,15 @@ const data = content.split("\n").map((x) => x.split(""))
 const maxY = data.length
 const maxX = data[0].length
 
-const isValidWord = (x: number, y: number, dx: number, dy: number): number => {
-    if ((y + dy * 3) >= maxY || (y + dy * 3) < 0) return 0 // check vertical boundary
-    if ((x + dx * 3) >= maxX || (x + dx * 3) < 0) return 0 // check horizontal boundary
-    return "XMAS".split("").every((letter, idx) => data[y + dy * idx][x + dx * idx] === letter) ? 1 : 0
+const isValidWord = (x: number, y: number, dx: number, dy: number): boolean => {
+    if ((y + dy * 3) >= maxY || (y + dy * 3) < 0) return false // check vertical boundary
+    if ((x + dx * 3) >= maxX || (x + dx * 3) < 0) return false // check horizontal boundary
+    return "XMAS".split("").every((letter, idx) => data[y + dy * idx][x + dx * idx] === letter)
 }
 
 // evaluate 8 possible directions from a starting point (x,y)
 const directions = [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
-const evalPosition = (x: number, y: number): number => directions.reduce((acc, dir) => acc + isValidWord(x, y, dir[1], dir[0]), 0)
+const evalPosition = (x: number, y: number): number => directions.reduce((acc, dir) => acc + (isValidWord(x, y, dir[1], dir[0]) ? 1 : 0), 0)
 
 let count = 0
 for (let y = 0; y < data.length; y++) {
--- day04/part2.ts
+++ day04/part2.ts
@@ -4,21 +4,21 @@ const data = content.split("\n").map((x) => x.split(""))
 const maxY = data.length
 const maxX = data[0].length
 
-const isValidWord = (x: number, y: number, dx: number, dy: number): number => {
-    if ((y + dy) >= maxY || (y + dy) < 0 || (y - dy) >= maxY || (y - dy) < 0) return 0 // check vertical boundary
-    if ((x + dx) >= maxX || (x + dx) < 0 || (x - dx) >= maxX || (x - dx) < 0) return 0 // check horizontal boundary
-    return "MAS".split("").every((letter, idx) => data[y + dy * (idx - 1)][x + dx * (idx - 1)] === letter) ? 1 : 0
+const isValidWord = (x: number, y: number, dx: number, dy: number): boolean => {
+    if ((y + dy) >= maxY || (y + dy) < 0 || (y - dy) >= maxY || (y - dy) < 0) return false // check vertical boundary
+    if ((x + dx) >= maxX || (x + dx) < 0 || (x - dx) >= maxX || (x - dx) < 0) return false // check horizontal boundary
+    return "MAS".split("").every((letter, idx) => data[y + dy * (idx - 1)][x + dx * (idx - 1)] === letter)
 }
 
 // evaluate only 4 possible diagonals from a starting point (x,y)
 const directions = [[1, 1], [1, -1], [-1, -1], [-1, 1]]
-const evalPosition = (x: number, y: number): number =>
-    (directions.reduce((acc, dir) => acc + isValidWord(x, y, dir[1], dir[0]), 0) == 2) ? 1 : 0
+const evalPosition = (x: number, y: number): boolean =>
+    directions.reduce((acc, dir) => acc + (isValidWord(x, y, dir[1], dir[0]) ? 1 : 0), 0) == 2
 
 let count = 0
 for (let y = 0; y < data.length; y++) {
     for (let x = 0; x < data[y].length; x++) {
-        count += evalPosition(x, y)
+        count += evalPosition(x, y) ? 1 : 0
     }
 }
 console.log(count)
