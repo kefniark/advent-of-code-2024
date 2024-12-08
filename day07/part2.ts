6ca1f556df65d606966455a82c2e4f5ef4725b08
Kevin Destrem <kefniark@gmail.com>
2024-12-08 05:32:48 +0000

    add day07

---
+++ day07/part2.ts
@@ -0,0 +1,22 @@
+const content = await Deno.readTextFile("./day07/input.txt")
+
+const operators = {
+    "0": (a: number, b: number) => a + b,
+    "1": (a: number, b: number) => a * b,
+    "2": (a: number, b: number) => parseInt(`${a}${b}`),
+}
+
+const getSortedOperators = (len: number, base: number) =>
+    [...Array(Math.pow(base, len)).keys()]
+        .map((_, i) => (i >>> 0).toString(base).padStart(len, "0"))
+
+const evaluate = (val: number[], ops: string[]) => val.slice(1).reduce((acc, num, i) => operators[ops[i]](acc, num), val[0])
+
+const res = content.split("\n")
+    .map((row) => {
+        const values = row.split(":")
+        return [parseInt(values[0]), values[1].trim().split(" ").map((i) => parseInt(i))] as [number, number[]]
+    })
+    .filter((row) => getSortedOperators(row[1].length - 1, 3).some((ops) => evaluate(row[1], ops) === row[0]))
+    .reduce((acc, row) => acc + row[0], 0)
+console.log(res)
